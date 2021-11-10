a2fa5d6962d63a5ab4cde859bc85b199773bd7a2
Plitea <dragosplitea@yahoo.com>
2021-11-11 01:03:34 +0200

    .

--- Cell.java
+++ Cell.java
@@ -1,30 +1,15 @@
-import java.sql.Timestamp;
-import java.util.Random;
-
 import static java.lang.Math.random;
 
 public class Cell {
 
     public int x;
     public int y;
-    public boolean isAlive = false;
-    public String type;
-    public static int count;
+    public int timesFeed = 0;
+    public boolean alive = true;
 
-    static Timestamp tstarve = new Timestamp(2000);
-    static Timestamp tfull = new Timestamp(5000);
-
-    public static void setTstarve(Timestamp tstarve) {
-        Cell.tstarve = tstarve;
-    }
-
-    public static void setTfull(Timestamp tfull){
-        Cell.tfull = tfull;
-    }
-
-    public Cell(int x, int y){
+    public Cell(int x, int y) {
         this.x = x;
-        this.y =y;
+        this.y = y;
     }
 
     public int getX() {
@@ -35,52 +20,42 @@ public class Cell {
         return y;
     }
 
-    public Cell(int x, int y, String type){
-        this.x = x;
-        this.y = y;
-        this.type = type;
+    public int getTimesFeed(int timesFeed) {
+        timesFeed = this.timesFeed;
+        return timesFeed;
     }
 
-    public boolean isAlive(){
-        return isAlive = true;
-    }
-
-    public boolean Dead(boolean isAlive){
-        this.isAlive = isAlive;
-        return false;
-    }
-
-    public int increase_food(){
-        if(Dead(isAlive) == false)
-            return Food.food = (int) (Food.food + (random()*((5-1)+1))+1);
-        return Food.food;
-    }
-
-    public void hungry(){
-        Dead(false);
-        increase_food();
-    }
-
-    Random random = new Random();
-
-    public void isFeed(){
-        Food.food = Food.food - 1;
-        Cell.count = Cell.count + 1;
-        System.out.println(Cell.count);
-        if(Cell.count == 10){
-            count = 0;
-            Cell cell = new Cell(random.nextInt(100),random.nextInt(100));
-            System.out.println("Coordonate X" + cell.getX());
-            System.out.println("Coordonate Y" + cell.getY());
-        }
-        emptyFood();
-    }
-
-    public boolean emptyFood(){
-        if(Food.food == 0){
-            System.out.println("Empty Food");
+    public boolean feed() {
+        if (Food.food > 0) {
+            this.timesFeed++;
+            Food.food--;
+            return true;        //feeded succesfully
+        } else
             return false;
-        }
-        return true;
     }
+
+
+    public boolean isFed() {
+        if (this.timesFeed >= 10)
+            return true;
+        else
+            return false;
+    }
+
+    public void die() {
+        this.alive = false;
+        Food.food = (int) (Food.food + (random() * ((5 - 1) + 1)) + 1);
+
+    }
+
+    public boolean isAlive() {
+        boolean state = this.alive;
+        return state;
+    }
+
+   /*public getNeighbors(int neighbors[4][4]){
+        //TODO later
+    }
+    */
+
 }
--- Main.java
+++ Main.java
@@ -8,19 +8,7 @@ public class Main {
         Scanner key = new Scanner(System.in);
         Food.food = 1;
         out.println(Food.food);
-        Cell cell = new Cell(10,10);
-        cell.Dead(false);
-        Cell cell1 = new Cell(11,12,"sex");
-        cell.hungry();
-        out.println(Food.food);
-        cell.hungry();
-        out.println(Food.food);
-        cell.isFeed();
-        out.println(Food.food);
-        cell.isFeed();
-        out.println(Food.food);
-        cell.isFeed();
-        out.println(Food.food);
+
 
     }
 }
