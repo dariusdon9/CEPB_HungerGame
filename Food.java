c731e1f3b0d3d2f442d478c8e49f50d756478490
dariusdon9 <91960095+dariusdon9@users.noreply.github.com>
2021-11-10 16:48:44 +0200

    Food.Java class for food declaration and consume
    I made the variable static, because I want in the future, to make a call, when I will have the graphic interface, and the text field, for setting the food

---
+++ Food.java
@@ -0,0 +1,11 @@
+public class Food {
+    static int food;
+
+    public static void setFood(int food) {
+        Food.food = food;
+    }
+   //Will be implemented after cell classes
+    /* public consume(){
+
+    }*/
+}
