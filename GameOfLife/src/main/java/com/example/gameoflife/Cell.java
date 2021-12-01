8e856e8de1ea0ff38b2c795db0fa63ff00d64e76
dariusdon9 <91960095+dariusdon9@users.noreply.github.com>
2021-12-01 16:06:26 +0200

    runnable
    runnable project

---
+++ GameOfLife/src/main/java/com/example/gameoflife/Cell.java
@@ -0,0 +1,80 @@
+package com.example.gameoflife;
+
+import static com.example.gameoflife.Food.*;
+import static java.lang.Math.random;
+
+public class Cell extends Thread{
+    public int x;
+    public int y;
+    public int food;
+    public Thread thread;
+    public String type;
+    public int timesFeed = 0;
+    public boolean alive = true;
+    public Cell(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public Cell(int x, int y,String type){
+        this.x = x;
+        this.y = y;
+        this.type =type;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(){
+        this.type = type;
+    }
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public  void setX(int x) {
+        this.x = x;
+    }
+
+    public  void setY(int y) {
+        this.y = y;
+    }
+
+    public int getTimesFeed(int timesFeed) {
+        timesFeed = this.timesFeed;
+        return timesFeed;
+    }
+
+    public boolean feed() {
+        if (Food.food > 0) {
+            this.timesFeed++;
+            Food.food--;
+            return true;        //feeded succesfully
+        } else
+            return false;
+    }
+
+
+    public boolean isFed() {
+        if (this.timesFeed >= 10)
+            return true;
+        else
+            return false;
+    }
+
+    public void die() {
+        this.alive = false;
+        Food.food = (int) (Food.food + (random() * ((5 - 1) + 1)) + 1);
+
+    }
+
+
+    public boolean isAlive1() {
+        boolean state = this.alive;
+        return state;
+    }
+}
---
+++ GameOfLife/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.gameoflife {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires java.desktop;
+    requires java.sql;
+
+    opens com.example.gameoflife to javafx.fxml;
+    exports com.example.gameoflife;
+}
